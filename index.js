1284d6aa431db5b72d7a4334249bc625cbd12e8a
Abdullah Khan <93679573+AbdullahKhan0800@users.noreply.github.com>
2023-10-12 17:48:18 +0500

    this is the last index.js change cmmit

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const path = require('path');
 
 const loginRoutes = require('./routes/loginRoutes')
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', loginRoutes)
+app.use('/static', express.static(path.join(__dirname, 'public')))
+// app.get('/admin/login' ,express.static(path.join(__dirname, 'public')));
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
 
